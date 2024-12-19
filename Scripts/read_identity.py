89c78f326dc054a206b5993d7970a1bf84206386
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-12-19 08:41:47 -0300

    fix: update path references in Python scripts for Docker compatibility
    This commit updates the path references in Python scripts to ensure compatibility with the new Docker structure. The changes were made to correctly resolve file paths when the application is containerized, preventing errors related to file access in the Docker environment.

--- Scripts/read_identity.py
+++ Scripts/read_identity.py
@@ -33,15 +33,15 @@ def main():
     """Reads an ICT FaceModel .json file and writes its mesh.
     """
     # Create a new FaceModel and load the model
-    id_coeffs, ex_coeffs = face_model_io.read_coefficients('../sample_data/sample_identity_coeffs.json')
-    face_model = face_model_io.load_face_model('../FaceXModel')
+    id_coeffs, ex_coeffs = face_model_io.read_coefficients('sample_data/sample_identity_coeffs.json')
+    face_model = face_model_io.load_face_model('FaceXModel')
     face_model.from_coefficients(id_coeffs, ex_coeffs)
 
     # Deform the mesh
     face_model.deform_mesh()
 
     # Write the deformed mesh
-    face_model_io.write_deformed_mesh('../sample_data_out/sample_identity.obj', face_model)
+    face_model_io.write_deformed_mesh('sample_data_out/sample_identity.obj', face_model)
 
 if __name__ == '__main__':
     main()
--- Scripts/sample_random.py
+++ Scripts/sample_random.py
@@ -33,7 +33,7 @@ def main():
     """Loads the ICT Face Mode and samples and writes 10 random identities.
     """
     # Create a new FaceModel and load the model
-    face_model = face_model_io.load_face_model('../FaceXModel')
+    face_model = face_model_io.load_face_model('FaceXModel')
 
     print("Writing 10 random identities...")
     for i in range(10):
@@ -42,7 +42,7 @@ def main():
         face_model.deform_mesh()
 
         # Write the deformed mesh
-        write_path = '../sample_data_out/random_identity{:02d}.obj'.format(i)
+        write_path = 'sample_data_out/random_identity{:02d}.obj'.format(i)
         face_model_io.write_deformed_mesh(write_path, face_model)
 
     print("Finished writing meshes.")
